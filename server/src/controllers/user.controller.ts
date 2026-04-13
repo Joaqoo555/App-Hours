c190c1d0db1c7c3ed3df0cc53730bc26664187f6
Joaqoo555 <joaquincarrera2018@gmail.com>
2026-04-13 15:59:24 -0300

    first commit

---
+++ server/src/controllers/user.controller.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from "express";
+
+const createUser = async (req: Request, res: Response) => {};
+
+const getUsers = async (req: Request, res: Response) => {};
+
+const getUserById = async (req: Request, res: Response) => {};
+
+const updateUser = async (req: Request, res: Response) => {};
+
+const deleteUser = async (req: Request, res: Response) => {};
+
+export { createUser, getUsers, getUserById, updateUser, deleteUser };
