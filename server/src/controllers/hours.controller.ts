311ed4cb5feb65f3f854c5c1ee17c66f164ee962
Joaqoo555 <joaquincarrera2018@gmail.com>
2026-04-26 19:56:10 -0300

    creacion de files

---
+++ server/src/controllers/hours.controller.ts
@@ -0,0 +1,13 @@
+import { Request, Response,  } from "express";
+
+const POST_hours = async (req: Request, res: Response) => {};
+
+const GET_hours = async (req: Request, res: Response) => {};
+
+const GET_hoursById = async (req: Request, res: Response) => {};
+
+const UPDATE_hours = async (req: Request, res: Response) => {};
+
+const DELETE_hours = async (req: Request, res: Response) => {};
+
+export { POST_hours, GET_hours, GET_hoursById, UPDATE_hours, DELETE_hours };
--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -1,13 +1,13 @@
 import { Request, Response } from "express";
 
-const createUser = async (req: Request, res: Response) => {};
+const POST_users = async (req: Request, res: Response) => {};
 
-const getUsers = async (req: Request, res: Response) => {};
+const GET_users = async (req: Request, res: Response) => {};
 
-const getUserById = async (req: Request, res: Response) => {};
+const GET_usersById = async (req: Request, res: Response) => {};
 
-const updateUser = async (req: Request, res: Response) => {};
+const UPDATE_users = async (req: Request, res: Response) => {};
 
-const deleteUser = async (req: Request, res: Response) => {};
+const DELETE_users = async (req: Request, res: Response) => {};
 
-export { createUser, getUsers, getUserById, updateUser, deleteUser };
+export { POST_users, GET_users, GET_usersById , UPDATE_users, DELETE_users };
